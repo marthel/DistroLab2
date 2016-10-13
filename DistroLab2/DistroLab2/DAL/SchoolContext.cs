550ad68638eb92f38d9e5d3b260041aed3188244
marthel <marthel@kth.se>
2016-10-13 13:56:42 +0200

    dbskit

---
+++ DistroLab2/DistroLab2/DAL/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using DistroLab2.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace DistroLab2.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
